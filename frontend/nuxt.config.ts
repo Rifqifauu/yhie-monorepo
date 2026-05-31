bf22f811b728183850e475158fff8ad153938a85
Rifqifauu <120528673+Rifqifauu@users.noreply.github.com>
2026-05-31 20:44:41 +0700

    commit

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,12 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
-  css: ['~/assets/css/main.css'],
+  modules: ["@nuxt/ui", "@nuxt/image", "@nuxtjs/i18n", "nuxt-aos"],
+  i18n: {
+    lazy: true,
+    langDir: "locales/",
+    locales: [
+      {
+        code: "id",
+        file: "id.json",
+        name: "Indonesia",
+      },
+      {
+        code: "en",
+        file: "en.json",
+        name: "English",
+      },
+    ],
+    defaultLocale: "id",
+    strategy: "prefix_except_default",
+  },
+  css: ["~/assets/css/main.css"],
   runtimeConfig: {
     public: {
-      apiBase: 'http://localhost:8000/api',
+      apiBase: "http://localhost:8000/api",
     },
   },
-})
+});
