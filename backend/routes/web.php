ecde876b37832e7eab40924537ed698263622483
Rifqifauu <120528673+Rifqifauu@users.noreply.github.com>
2026-06-04 21:54:53 +0700

    Update web.php

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,5 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::get('/login', [AuthController::class, 'login']);
-
+Route::post("/login", [AuthController::class, "login"]);
